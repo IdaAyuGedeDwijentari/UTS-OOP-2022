66c305a820db4a0ce8c65c245b713ad04e696bd2
IdaAyuGedeDwijentari <101305025+IdaAyuGedeDwijentari@users.noreply.github.com>
2022-04-27 08:40:38 +0800

    Add files via upload

--- UTSOOP.java
+++ UTSOOP.java
@@ -20,38 +20,41 @@ public class UTSOOP {
             
             if (n==1){ 
                 if (nama.size() == 0){
-                System.out.println("##### Daftar Pengunjung Kebun Binatang #####");
-                System.out.println("\n\n Belum Ada Pengunjung");
+                    System.out.println("");                
+                    System.out.println("\n##### Daftar Pengunjung Kebun Binatang #####\n");
+                    System.out.println("Belum Ada Pengunjung");
+                    System.out.println("");
+                    System.out.println("Silahkan pilih menu di bawah ini....");                    
                     } else {
-                System.out.println("\n\n##### Data Nama Pengunjung #####");
-                for (int i = 0; i < nama.size(); i++){
-                System.out.println((i+1)+". "+nama.get(i));
-                System.out.println("");
-                System.out.println("Total Jumlah Pengunjung: "+nama.size()+" pengunjung");                
+                    System.out.println("\n##### Data Nama Pengunjung #####");
+                    for (int i = 0; i < nama.size(); i++){
+                    System.out.println((i+1)+". "+nama.get(i));        
                     }
-                }
-                }else if (n==2){
+                    System.out.println("");
+                    System.out.println("Total Jumlah Pengunjung: "+nama.size()+" pengunjung");
+                    System.out.println("");
+                    System.out.println("Silahkan pilih menu di bawah ini....");                
+                    }
+                    }else if (n==2){
                     System.out.println("");
                     System.out.println("Isi Data Nama Pengunjung");
                     System.out.print("Nama Pengunjung :");
                     String Np = input.next();
                     nama.add(Np);
                     System.out.println("");
-                    System.out.println("Data pengunjung telah di record.....");
+                    System.out.println("Data pengunjung telah di record, Silahkan pilih menu di bawah ini....");                   
                     }else if (n==3){
                     System.out.println("\n\n");                    
                     System.out.println("############## TERIMAKASI TELAH BERKUNJUNG #################");
                     System.out.println("\n\n"); 
                     }else if (n>3){
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     System.out.print("Opsi Pilihan tidak terdaftar, silahkan pilih 1,2 atau 3... ");
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");                    
                     }
-                System.out.println("");
-                System.out.println("Klik enter untuk melanjutkan...");
             }while (n!=3);     
         }   
-    }       
+    }
     
    
     
